b5e119b7910072c6843ff1c84d4e26814ce90e4b
elisapercalli <67218140+elisapercalli@users.noreply.github.com>
2022-10-13 11:46:03 +0200

    Update runMASFit.sh

--- runMASFit.sh
+++ runMASFit.sh
@@ -1,1 +1,1 @@
-python3 MASFit_2.1.py input_MASFit_2.txt inputFlux.txt inputCores.txt bkg_geo.txt bkg_acc.txt bkg_alpha.txt bkg_fn.txt bkg_Li9.txt
+python3 MASFit_2.py input_MASFit_2.txt inputFlux.txt inputCores.txt bkg_geo.txt bkg_acc.txt bkg_alpha.txt bkg_fn.txt bkg_Li9.txt
