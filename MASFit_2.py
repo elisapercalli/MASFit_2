e3263c5f709d2ae0e40ddb699a1bc9229e7306b4
elisapercalli <67218140+elisapercalli@users.noreply.github.com>
2022-10-14 12:27:09 +0200

    Update MASFit_2.py
    Added world reactors background and correlation matrix

--- MASFit_2.py
+++ MASFit_2.py
@@ -11,6 +11,7 @@ import numpy as np
 from iminuit import Minuit
 import sys
 import MASFit_func_2 as fn
+import seaborn as sn
 
 #Reading parameters from file
 InputTextFile = sys.argv[1]
@@ -21,6 +22,7 @@ InputBkgFile2 = sys.argv[5]
 InputBkgFile3 = sys.argv[6]
 InputBkgFile4 = sys.argv[7]
 InputBkgFile5 = sys.argv[8]
+InputBkgFile6 = sys.argv[9]
 
 #Energy and other parameters
 v=fn.ReadParams(InputTextFile)
@@ -66,28 +68,32 @@ sigma_B_fn=v[28]/100
 sigma_sh_fn=v[29]/100
 sigma_B_Li9=v[30]/100
 sigma_sh_Li9=v[31]/100
-corr=bool(v[32])
+sigma_B_WR=v[32]/100
+sigma_sh_WR=v[33]/100
+Scan=bool(v[34])
+corr=bool(v[35])
 #Fit parameters
-Fix_M21=bool(v[33])
-Fix_M3l=bool(v[34])
-Fix_T13=bool(v[35])
-Fix_T12=bool(v[36])
-Fix_N=bool(v[37])
-Fix_a=bool(v[38])
-Fix_b=bool(v[39])
-Fix_c=bool(v[40])
+Fix_M21=bool(v[36])
+Fix_M3l=bool(v[37])
+Fix_T13=bool(v[38])
+Fix_T12=bool(v[39])
+Fix_N=bool(v[40])
+Fix_a=bool(v[41])
+Fix_b=bool(v[42])
+Fix_c=bool(v[43])
 
 #Rate for normalization
-IBD_r=v[41]
-Norm_geo=v[42]/IBD_r*Ntot
-Norm_acc=v[43]/IBD_r*Ntot
-Norm_Li9=v[44]/IBD_r*Ntot
-Norm_fn=v[45]/IBD_r*Ntot
-Norm_alpha=v[46]/IBD_r*Ntot
+IBD_r=v[44]
+Norm_geo=v[45]/IBD_r*Ntot
+Norm_acc=v[46]/IBD_r*Ntot
+Norm_Li9=v[47]/IBD_r*Ntot
+Norm_fn=v[48]/IBD_r*Ntot
+Norm_alpha=v[49]/IBD_r*Ntot
+Norm_WR=v[50]/IBD_r*Ntot
 
 #Check for real or ideal baseline
-if(v[47]!=0):
-    Dist=v[47]
+if(v[51]!=0):
+    Dist=v[51]
 
 #Reading anti-nu flux from file
 F=fn.ReadFlux(InputFluxFile, Nbin)
@@ -96,6 +102,8 @@ Bg_acc=fn.ReadFlux(InputBkgFile2, Nbin)
 Bg_alpha=fn.ReadFlux(InputBkgFile3, Nbin)
 Bg_fn=fn.ReadFlux(InputBkgFile4, Nbin)
 Bg_Li9=fn.ReadFlux(InputBkgFile5, Nbin)
+Bg_WR=fn.ReadFlux(InputBkgFile6, Nbin)
+
 
 #Normalizaion of backgrounds
 Bg_geo*=(Norm_geo/np.sum(Bg_geo))
@@ -103,7 +111,8 @@ Bg_acc*=(Norm_acc/np.sum(Bg_acc))
 Bg_alpha*=(Norm_alpha/np.sum(Bg_alpha))
 Bg_fn*=(Norm_fn/np.sum(Bg_fn))
 Bg_Li9*=(Norm_Li9/np.sum(Bg_Li9))
-Bg_tot=Bg_acc+Bg_alpha+Bg_fn+Bg_geo+Bg_Li9
+Bg_WR*=(Norm_WR/np.sum(Bg_WR))
+Bg_tot=Bg_acc+Bg_alpha+Bg_fn+Bg_geo+Bg_Li9+Bg_WR
 
 #Creation of anti-nu NO flux
 bin=(Emax-Emin)/Nbin
@@ -136,25 +145,24 @@ for i in range(0,M):
 
     #Definition of the Chi squared
     def least_squares_syst(T13, T12, M21, M3l, ord, N, A, B, C,alpha_C,alpha_D,a_R1,a_R2,a_R3,a_R4,a_R5,a_R6,a_R7,a_R8,a_R9,\
-         alpha_B1,alpha_B2,alpha_B3,alpha_B4,alpha_B5):
+         alpha_B1,alpha_B2,alpha_B3,alpha_B4,alpha_B5,alpha_B6):
         yT = fn.Flux_teo(e, F, ord, T13, T12, M21, M3l, Dist, Weights, N, A, B, C)    #without background
-        V=3/(1/yMis+2/(yT+Bg_tot))
         weig_sum=Weights[0]*a_R1+Weights[1]*a_R2+Weights[2]*a_R3+Weights[3]*a_R4+Weights[4]*a_R5+Weights[5]*a_R6+Weights[6]*a_R7+Weights[7]*a_R8+Weights[8]*a_R9
-        z_num=(yMis-yT*(1+alpha_C+alpha_D+weig_sum)-Bg_geo*(1+alpha_B1)-Bg_acc*(1+alpha_B2)-Bg_alpha*(1+alpha_B3)-Bg_fn*(1+alpha_B4)-Bg_Li9*(1+alpha_B5))**2
-        z_den=yMis+(sigma_b2b*yT)**2+(sigma_sh_geo*Bg_geo)**2+(sigma_sh_acc*Bg_acc)**2+(sigma_sh_alpha*Bg_alpha)**2+(sigma_sh_fn*Bg_fn)**2+(sigma_sh_Li9*Bg_Li9)**2
+        z_num=(yMis-yT*(1+alpha_C+alpha_D+weig_sum)-Bg_geo*(1+alpha_B1)-Bg_acc*(1+alpha_B2)-Bg_alpha*(1+alpha_B3)-Bg_fn*(1+alpha_B4)-Bg_Li9*(1+alpha_B5)-Bg_WR*(1+alpha_B6))**2
+        z_den=yMis+(sigma_b2b*yT)**2+(sigma_sh_geo*Bg_geo)**2+(sigma_sh_acc*Bg_acc)**2+(sigma_sh_alpha*Bg_alpha)**2+(sigma_sh_fn*Bg_fn)**2+(sigma_sh_Li9*Bg_Li9)**2+(sigma_sh_WR*Bg_WR)**2
         CHI=np.sum(z_num/z_den)
         pull_a=((A-a)/sigma_a)**2
         pull_b=((B-b)/sigma_b)**2
         pull_c=((C-c)/sigma_c)**2
         pull_syst=(alpha_C/sigma_alphaC)**2+(alpha_D/sigma_alphaD)**2
         pull_syst_cores=(a_R1**2+a_R2**2+a_R3**2+a_R4**2+a_R5**2+a_R6**2+a_R7**2+a_R8**2+6*a_R9**2)/sigma_alphaR**2
-        pull_bkg=(alpha_B1/sigma_B_geo)**2+(alpha_B2/sigma_B_acc)**2+(alpha_B3/sigma_B_alpha)**2+(alpha_B4/sigma_B_fn)**2+(alpha_B5/sigma_B_Li9)**2
+        pull_bkg=(alpha_B1/sigma_B_geo)**2+(alpha_B2/sigma_B_acc)**2+(alpha_B3/sigma_B_alpha)**2+(alpha_B4/sigma_B_fn)**2+(alpha_B5/sigma_B_Li9)**2+(alpha_B6/sigma_B_WR)**2
         return CHI+pull_a+pull_b+pull_c+pull_syst+pull_syst_cores+pull_bkg
     least_squares_syst.errordef = Minuit.LEAST_SQUARES
 
     #Fit NO on NO
-    m = Minuit(least_squares_syst, T13=Theta13_NO,T12=Theta12+0.152,M21=DeltaM21,M3l=DeltaM31_NO, ord=1, N=Ntot,A=a,B=b,C=c,alpha_C=0,alpha_D=0,\
-        a_R1=0, a_R2=0, a_R3=0, a_R4=0, a_R5=0, a_R6=0, a_R7=0, a_R8=0, a_R9=0, alpha_B1=0,alpha_B2=0,alpha_B3=0,alpha_B4=0,alpha_B5=0)
+    m = Minuit(least_squares_syst, T13=Theta13_NO,T12=Theta12,M21=DeltaM21,M3l=DeltaM31_NO, ord=1, N=Ntot,A=a,B=b,C=c,alpha_C=0,alpha_D=0,\
+        a_R1=0, a_R2=0, a_R3=0, a_R4=0, a_R5=0, a_R6=0, a_R7=0, a_R8=0, a_R9=0, alpha_B1=0,alpha_B2=0,alpha_B3=0,alpha_B4=0,alpha_B5=0,alpha_B6=0)
 
     #Fit parameters
     m.fixed["ord"]=True
@@ -170,8 +178,8 @@ for i in range(0,M):
     m.hesse()
 
     #Fit IO su NO
-    n = Minuit(least_squares_syst, T13=Theta13_IO,T12=Theta12+0.152,M21=DeltaM21,M3l=DeltaM32_IO, ord=0, N=Ntot,A=a,B=b,C=c,alpha_C=0,alpha_D=0, \
-        a_R1=0, a_R2=0, a_R3=0, a_R4=0, a_R5=0, a_R6=0, a_R7=0, a_R8=0, a_R9=0, alpha_B1=0,alpha_B2=0,alpha_B3=0,alpha_B4=0,alpha_B5=0)
+    n = Minuit(least_squares_syst, T13=Theta13_IO,T12=Theta12,M21=DeltaM21,M3l=DeltaM32_IO, ord=0, N=Ntot,A=a,B=b,C=c,alpha_C=0,alpha_D=0, \
+        a_R1=0, a_R2=0, a_R3=0, a_R4=0, a_R5=0, a_R6=0, a_R7=0, a_R8=0, a_R9=0, alpha_B1=0,alpha_B2=0,alpha_B3=0,alpha_B4=0,alpha_B5=0,alpha_B6=0)
 
     #Fit parameters
     n.fixed["ord"]=True
@@ -190,7 +198,7 @@ for i in range(0,M):
 
 if(Fluc==False):
     #Plot parameters scan
-    if(corr==True):
+    if(Scan==True):
         fig_Z3,ax_Z3 = plt.subplots(1,1)
         xm,ym,rm = m.mnprofile('M3l',bound=5, subtract_min=False)
         xn,yn,rn = n.mnprofile('M3l',bound=5, subtract_min=False)
@@ -249,6 +257,15 @@ if(Fluc==False):
     file_par.write(f"\n\nDeltaChi^2 = {n.fval-m.fval:.2f}")
     file_par.close()
 
+    #Correlation matrix
+    plt.figure(figsize=(14,10))
+    lab=["T12","M21","M3l","A","B","C","a_C","a_D","a_R1","a_R2","a_R3","a_R4","a_R5","a_R6","a_R7","a_R8","a_R9","a_B1","a_B2","a_B3","a_B4","a_B5","a_B6"]
+    corr = m.covariance.correlation()
+    corr2=np.delete(corr,(0,4,5),axis=0)
+    corr3=np.delete(corr2,(0,4,5),axis=1)
+    sn.heatmap(corr3, xticklabels=lab, yticklabels=lab,fmt=".2f", cmap="BrBG",linewidths=.5,annot=True)
+    plt.savefig("corr_matrix.png")
+
     #Plot
     plt.figure(figsize=(12, 8))
     plt.title("Fit su NO",fontsize=16)
@@ -275,7 +292,7 @@ if(Fluc==False):
     plt.grid()
     plt.xlabel("E_vis [MeV]",fontsize=14)
     plt.ylabel("N events",fontsize=14)
-    plt.savefig("MASFit_plot.png")
+    plt.savefig("MASFit_plot.pdf")
 
 else:
     #checking for outliers and deleting them
@@ -285,7 +302,7 @@ else:
     i=0
     j=0
     while (i<(M-j)):
-        if (D_CHI[i]>3*std_CHI+mean_CHI):
+        if (D_CHI[i]>5*std_CHI+mean_CHI):
             D_CHI.pop(i)
             i-=1
             j+=1
@@ -298,8 +315,8 @@ else:
     plt.grid()
     plt.xlabel(f"$\\Delta$ $\\chi^2$")
     plt.ylabel("N events")
-    plt.savefig("MASFit_CHI.png")
+    plt.savefig("MASFit_CHI.pdf")
 
 print("Computational time: ",f"{(tm.time()-start_tm):.2f}"," s")
-if(v[48]==True):
+if(v[52]==True):
     plt.show()
